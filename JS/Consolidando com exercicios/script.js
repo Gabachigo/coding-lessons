2d825be927d4231849fa5f200b8eb6dea0c4493f
Davi Almeida <davi.castelo.branco.almeida@gmail.com>
2023-01-16 17:45:05 +0000

    backup

--- JS/Consolidando com exercicios/script.js
+++ JS/Consolidando com exercicios/script.js
@@ -1,18 +1,59 @@
 const bookByCategory = [
   {
     category: "Big money",
-    book: [],
+    book: [
+      {
+        title: "Lottery",
+        author: "Danquavious Midlenut"
+      },
+      {
+        title: "Big money 2.0",
+        author: "Quagwavious"
+      },
+      {
+        title: "Yeat keeps on evolving",
+        author:"Meat"
+      }
+    ],
   },
   {
     category: "Big poor",
-    book: [],
+    book: [
+      {
+        title: "Prision",
+        author: "Danquavious Midlenut"
+      },
+      {
+        title: "I sharted",
+        author: "Quagwavious"
+      },
+      {
+        title: "Yeat keeps on losing money",
+        author:"Meat"
+      }
+    ],
   },
   {
-    category: "Big walter",
-    book: [],
-  },
-  {
-    category: "Big egg",
-    book: [],
-  },
-]
+    category: "Big emotions",
+    book: [
+      {
+        title: "I sharting tears",
+        author: "Danquavious Midlenut"
+      },
+      {
+        title: "Danquavious foul Fanquavious",
+        author: "Quagwavious"
+      },
+      {
+        title: "Food makes me cry",
+        author:"Meat"
+      }
+    ],
+  }
+];
+
+let totalCategories = function total() {
+  for (let category of bookByCategory) {
+    total += 1
+  }
+};
